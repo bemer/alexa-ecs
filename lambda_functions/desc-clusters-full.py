8cb46026f48eb192ab7553de10d68f2ae8ee70e3
JP Santana <jpsant@amazon.com>
2017-01-05 16:08:16 -0200

    added replace '\' to cluster list

--- lambda_functions/desc-clusters-full.py
+++ lambda_functions/desc-clusters-full.py
@@ -5,23 +5,21 @@ from __future__ import print_function
 import boto3
 import json
 
-
 client = boto3.client('ecs')
 
-def lambda_handler(event, context):
 
+def lambda_handler(event, context):
     response = client.list_clusters(maxResults=100)
     clusterArns = response['clusterArns']
     clusterList = []
 
     for i in clusterArns:
         response = client.describe_clusters(clusters=[i])
-        clusterList.insert(0,response['clusters'][0]['clusterName'])
-
+        clusterList.insert(0, response['clusters'][0]['clusterName'])
 
     clusterList = json.dumps(clusterList)
     print(clusterList)
 
     clusterList = ''.join(clusterList)
 
-    return clusterList
+    return clusterList.replace("\"", " ")
