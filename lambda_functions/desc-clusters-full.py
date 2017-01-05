6af933f422ee95e366c50efc3c440946fc600a30
Bruno Emer <brunoemer@gmail.com>
2017-01-05 15:38:37 -0200

    describe clusters full

--- lambda_functions/desc-clusters-full.py
+++ lambda_functions/desc-clusters-full.py
@@ -1,17 +1,27 @@
+# -*- coding: UTF-8
+
+from __future__ import print_function
+
 import boto3
 import json
 
 
+client = boto3.client('ecs')
 
-# This function lists the services running in a specific cluster.
 def lambda_handler(event, context):
-    client = boto3.client('ecs')
 
     response = client.list_clusters(maxResults=100)
     clusterArns = response['clusterArns']
+    clusterList = []
 
     for i in clusterArns:
         response = client.describe_clusters(clusters=[i])
-        print response['clusters'][0]['clusterName']
+        clusterList.insert(0,response['clusters'][0]['clusterName'])
 
-    return "OK"
+
+    clusterList = json.dumps(clusterList)
+    print(clusterList)
+
+    clusterList = ''.join(clusterList)
+
+    return clusterList
